5021658e46ad656fb9277713170565775ba3ebf0
Aleksc <aleksandar.cosic1@yahoo.com>
2018-07-13 20:38:22 +0200

    Zadatak 2

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Cars</title>
+        <title>Welcome to Cars Project</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -79,12 +79,10 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                Welcome to Cars Project
+                    About us
                 </div>
 
-                <div class="links">
-                    <a href="about">About</a>
-                </div>
+
             </div>
         </div>
     </body>
